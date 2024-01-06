3a19496e21444a0b8cbc5918fc567c9b43d44363
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-01-06 16:31:39 -0500

    Hardware Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/hardware.java
@@ -1,2 +1,69 @@
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.DCMotorEx;
+import com.arcrobotics.ftclib.hardware.motors.Encoder;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+
+@config
 public class hardware {
+    public DCMotorEx frontLeftMotor;
+    public Motor.Encoder frontLeftMotorEncoder;
+    public DCMotorEx backLeftMotor;
+    public Motor.Encoder backLeftMotorEncoder;
+
+    public DCMotorEx frontRightMotor;
+    public Motor.Encoder frontRightMotorEncoder;
+    public DCMotorEx backRightMotor;
+    public Motor.Encoder backRightMotorEncoder;
+
+    public DCMotorEx leftSlideMotor;
+    public DCMotorEx rightSlideMotor;
+    public DCMotorEx intakeMotor;
+    public Motor.Encoder intakeMotorEncoder;
+
+    public Motor.Encoder odometryLeftEncoder;
+    public Motor.Encoder odometryRightEncoder;
+    public Motor.Encoder odometryCenterEncoder;
+
+    private HardwareMap hardwareMap;
+
+    public void init(final HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+
+        frontLeftMotor = new DCMotorEx(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeftMotor = new DCMotorEx(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRightMotor = new DCMotorEx(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backRightMotor = new DCMotorEx(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+
+
+        leftSlideMotor = new DCMotorEx(hardwareMap, "leftSlide", Motor.GoBILDA.RPM_435);
+        rightSlideMotor = new DCMotorEx(hardwareMap, "rightSlide", Motor.GoBILDA.RPM_435);
+        intakeMotor = new DCMotorEx(hardwareMap, "intakeMotor", Motor.GoBILDA.RPM_117);
+
+        // encoders
+        frontLeftMotorEncoder = new Encoder(hardwareMap, "leftFront");
+        backLeftMotorEncoder = new Encoder(hardwareMap, "leftBack");
+        frontRightMotorEncoder = new Encoder(hardwareMap, "rightFront");
+        backRightMotorEncoder = new Encoder(hardwareMap, "rightBack");
+
+        intakeMotorEncoder = new Encoder(hardwareMap, "intakeMotor");
+
+        odometryLeftEncoder = new Encoder(hardwareMap, "leftSlide");
+        odometryRightEncoder = new Encoder(hardwareMap, "rightSlide");
+        odometryCenterEncoder = new Encoder(hardwareMap, "centerOdo");
+
+
+
+
+        );
+    }
 }
